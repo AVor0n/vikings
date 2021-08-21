cf4d3517ca8b57e3cd197133f3840d752f99ca14
Andrey Voronin <AVor0n@yandex.ru>
2021-08-21 22:03:19 +0500

    feat(main.js): add scroller for heroes section

--- js/main.js
+++ js/main.js
@@ -5,4 +5,18 @@ $(function () {
             showinfo: 0
         }
     });
+
+    $('.heroes__slider-img').slick({
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        asNavFor: '.heroes__slider-text',
+        prevArrow: '<button type="button" class="slick-btn slick-prev"><img src="images/arrow-l.png" alt="prev"></button>',
+        nextArrow: '<button type="button" class="slick-btn slick-next"><img src="images/arrow-r.png" alt="next"></button>'
+    });
+    $('.heroes__slider-text').slick({
+        slidesToShow: 1,
+        fade: true,
+        arrows: false,
+        asNavFor: '.heroes__slider-img',
+    });
 });
